ecd695f40a1c2f6cabe8642e43d453dcce2c3b8f
Veni1191 <venisha2025@outlook.com>
2025-10-16 18:28:47 +1030

    Add files via upload

--- game.py
+++ game.py
@@ -9,6 +9,7 @@ TITLE = "Space Shooter"
 score = 0
 lives = 3
 is_game_over = False
+speed = 5
 enemies = []
 bullets = []
 
@@ -26,6 +27,61 @@ for i in range(8):
 def display_score():
     screen.draw.text(f"score:{score}",(50,30))
     screen.draw.text(f"lives:{lives}",(50,60))
+
+#creating bullets
+def on_key_down(key):
+    if key == keys.SPACE:
+        bullet = Actor("bullet")
+        bullet.x = spacecraft.x
+        bullet.y = spacecraft.y - 50
+        bullets.append(bullet)
+        
+#function to draw game state
+def draw():
+    if lives > 0: 
+        screen.clear()
+        screen.fill("#006494")
+        spacecraft.draw()
+        for enemy in enemies:
+            enemy.draw()
+        for bullet in bullets:
+            bullet.draw()
+        display_score()
+    else:
+        game_over_screen()
+
+#function to update game state
+def update():
+    global score, lives
+    #move ship left or right
+    if keyboard.left:
+        spacecraft.x -= speed
+        if spacecraft.x <= 0:
+            spacecraft.x = 0
+    if keyboard.right:
+        spacecraft.x += speed
+        if spacecraft.x >= WIDTH:
+            spacecraft.x = WIDTH
+    
+    #moving the bullets
+    for bullet in bullets:
+        if bullet.y <= 0:
+            bullets.remove(bullet)
+        else: 
+            bullet.y -= 10
+    
+    #moving enemies
+    #move_down = False
+    for enemy in enemies:
+        enemy.y += 5
+        if enemy.y > HEIGHT:
+            enemy.x = randint(0,1120)
+            enemy.y = randint(-100,0)
+            
+
+
+
+            
 
 
 
